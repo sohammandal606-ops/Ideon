6dcc4aaab02953e41f64243b677d52765cb513d7
soham <sohammandal605@gmail.com>
2026-08-19 11:54:09 +0530

    new agent add

--- backend/workflows/agents/__init__.py
+++ backend/workflows/agents/__init__.py
@@ -1,3 +1,5 @@
 from .idea_validator import idea_validator_node
 
 __all__ = ["idea_validator_node"]
+
+
--- backend/workflows/workflow.py
+++ backend/workflows/workflow.py
@@ -2,7 +2,9 @@ from langgraph.graph import END, StateGraph
 
 from workflows.agents.idea_validator import idea_validator_node
 from workflows.state import StartupState
-
+from workflows.agents.market_research import market_research_node
+from workflows.agents.compititor_analysis import competitor_analysis_node
+from workflows.agents.business_model import business_model_node
 
 def create_workflow():
     """
@@ -11,14 +13,52 @@ def create_workflow():
     workflow = StateGraph(StartupState)
 
     # Add nodes
-    workflow.add_node("idea_validator", idea_validator_node)
-
-    # Add edges
+    workflow.add_node(
+        "idea_validator", 
+         idea_validator_node
+         )
+    
+    workflow.add_node(
+       "market_research",
+       idea_validator_node,
+   )
+    workflow.add_node(
+            "market_research",
+            market_research_node,
+        )
+    workflow.add_node(
+        "competitor_analysis",
+        competitor_analysis_node,
+    )
+    workflow.add_node(
+        "business_model",
+        business_model_node,
+    )
     workflow.set_entry_point("idea_validator")
+#paralle execution..
 
-    # For now, end after idea validation since we are moving step-by-step
-    workflow.add_edge("idea_validator", END)
+    workflow.add_edge(
+    "idea_validator",
+    "market_research",
+    )
+    
+    workflow.add_edge(
+        "idea_validator",
+        "competitor_analysis",
+    )
+    workflow.add_edge(
+        "market_research",
+        "business_model",
+    )
 
+    workflow.add_edge(
+        "competitor_analysis",
+        "business_model",
+    )
+    workflow.add_edge(
+        "business_model",
+        END,
+    )
     return workflow.compile()
 
 
